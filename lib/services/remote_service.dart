1a7f82672574bc1b170e7bebf1550388d2cccebd
kasun <kasunpradeep.ds@gmail.com>
2024-06-27 23:22:17 +0530

    service

---
+++ lib/services/remote_service.dart
@@ -0,0 +1,18 @@
+import 'package:api_curd/models/post.dart';
+import 'package:http/http.dart' as http;
+
+class RemoteService {
+  
+  Future<List<Post>?> getPost() async {
+    var client = http.Client();
+
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await client.get(uri);
+
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postFromJson(json);
+    }
+    return null;
+  }
+}
